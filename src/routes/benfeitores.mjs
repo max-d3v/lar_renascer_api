a4d2063d95529b915bf518dcd9a0ec4b663f4576
max-d3v <max.maul.desenvolvedor@gmail.com>
2024-06-10 08:20:46 -0300

    benfeitores register

--- src/routes/benfeitores.mjs
+++ src/routes/benfeitores.mjs
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 import { checkSchema } from "express-validator";
 import { matchedData,validationResult } from "express-validator";
 import { handleError } from "../utils/helpers.mjs";
-
+import { benfeitoresSearch, benfeitorRegisterFisica, benfeitorRegisterJuridica } from "../utils/validationSchemas.mjs";
 const prisma = new PrismaClient();
 
 const router = Router();
@@ -45,6 +45,50 @@ router.post("/benfeitores", checkSchema(benfeitoresSearch), async (req, res) => 
     return handleError(res, "Erro inesperado.");
 });
 
+
+router.post("/benfeitores/registerjuridica", checkSchema(benfeitorRegisterJuridica), async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(200).send({status: "error", "message": errors.array()});
+    }
+    const { cnpj, razaoSocial, telefone } = matchedData(req);
+    try {
+        const benfeitor = await prisma.benfeitores.create({
+            data: {
+                cnpj,   
+                razaoSocial: razaoSocial,
+                telefone,
+                tipo: "juridico"
+            }
+        });
+        console.log(benfeitor)
+        return res.status(200).send({status: "success", data: benfeitor});
+    } catch (error) {
+        console.log(error);
+        return handleError(res, error);
+    }
+})
+
+router.post("/benfeitores/registerfisica", checkSchema(benfeitorRegisterFisica), async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(200).send({status: "error", "message": errors.array()});
+    }
+    const { nome, telefone, cpf } = matchedData(req);
+    try {
+        const benfeitor = await prisma.benfeitores.create({
+            data: {
+                cpf,
+                nome,
+                telefone,
+                tipo: "fisico"
+            }
+        });
+        return res.status(200).send({status: "success", data: benfeitor});
+    } catch (error) {
+        return handleError(res, error);
+    }
+})
 
 
 
