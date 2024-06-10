e222e00e4920ca3aeb363e2430324e91538ae665
max-d3v <max.maul.desenvolvedor@gmail.com>
2024-06-10 08:21:04 -0300

    benfeitores validation and validation functions

--- src/utils/helpers.mjs
+++ src/utils/helpers.mjs
@@ -32,3 +32,45 @@ export const hashSenha = (senha) => {
     const salt = bcrypt.genSaltSync(saltRounds);
     return bcrypt.hashSync(senha, salt)
 }
+
+export const validaCNPJ = (cnpj) => {
+    var b = [ 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 ]
+    var c = String(cnpj).replace(/[^\d]/g, '')
+    
+    if(c.length !== 14)
+        return false
+
+    if(/0{14}/.test(c))
+        return false
+
+    for (var i = 0, n = 0; i < 12; n += c[i] * b[++i]);
+    if(c[12] != (((n %= 11) < 2) ? 0 : 11 - n))
+        return false
+
+    for (var i = 0, n = 0; i <= 12; n += c[i] * b[i++]);
+    if(c[13] != (((n %= 11) < 2) ? 0 : 11 - n))
+        return false
+
+    return true
+}
+
+export const validaCPF = (strCPF) => {
+    var Soma;
+    var Resto;
+    Soma = 0;
+  if (strCPF == "00000000000") return false;
+
+  for (var i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+  Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+
+  Soma = 0;
+    for (var i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+    return true;
+}
--- src/utils/validationSchemas.mjs
+++ src/utils/validationSchemas.mjs
@@ -1,4 +1,6 @@
-//Validation schemas using express-validator
+import { validaCNPJ, validaCPF } from "./helpers.mjs"
+
+
 export const acolhidaSearch = {
     acolhida: {
         notEmpty: {
@@ -20,4 +22,63 @@ export const benfeitoresSearch = {
         },
     }
 }
+
+export const benfeitorRegisterFisica = {
+    nome: {
+        notEmpty: {
+            errorMessage: "Nome não fornecido!",
+        },
+    },
+    telefone: {
+        isMobilePhone: {
+            errorMessage: "Número de telefone inválido!"
+        },
+        notEmpty: {
+            errorMessage: "Número de telefone não fornecido!",
+        },
+    },
+    cpf: {
+        notEmpty: {
+            errorMessage: "CPF não fornecido!",
+        },
+        custom: {
+            options: (value) => {
+                if (!validaCPF(value)) {
+                    throw new Error('CPF inválido!');
+                }
+                return true;
+            }
+        }
+    }
+}
+
+export const benfeitorRegisterJuridica = {
+    razaoSocial: {
+        notEmpty: {
+            errorMessage: "Nome não fornecido!",
+        },
+    },
+    telefone: {
+        isMobilePhone: {
+            errorMessage: "Número de telefone inválido!"
+        },
+        notEmpty: {
+            errorMessage: "Número de telefone não fornecido!",
+        },
+    },
+    cnpj: {
+        notEmpty: {
+            errorMessage: "CPF não fornecido!",
+        },
+        custom: {
+            options: (value) => {
+                if (!validaCNPJ(value)) {
+                    throw new Error('CNPJ inválido!');
+                }
+                return true;
+            }
+        }
+    }
+}
+
 // ----------------------------
